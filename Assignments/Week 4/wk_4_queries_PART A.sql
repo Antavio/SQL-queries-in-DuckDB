c9b17578d2548eb71df3572988861a5a536362a4
Antavio <njuguna13@gmail.com>
2025-10-19 14:29:46 +0200

    Update to Week 4 Part A assignment

--- Assignments/Week 4/wk_4_queries_PART A.sql
+++ Assignments/Week 4/wk_4_queries_PART A.sql
@@ -7,18 +7,20 @@ WITH yellow_first_cleanup AS
 ), 
 yellow_second_cleanup AS
  (
-    SELECT VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, UPPER(TRIM(store_and_fwd_flag)) store_and_fwd_flag,
+    SELECT VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, 
+    CASE WHEN UPPER(TRIM(store_and_fwd_flag)) IN ('Y','N') THEN UPPER(TRIM(store_and_fwd_flag))
+    ELSE NULL
+    END AS store_and_fwd_flag,
     ROUND(trip_distance,3) trip_distance,
     ROUND(total_amount,3) total_amount
     FROM yellow_first_cleanup
-    --WHERE store_and_fwd_flag IS NOT NULL
  ), 
  range_of_customers AS
  (
     SELECT VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, CASE
     WHEN passenger_count = 1 THEN 'Solo'
     WHEN passenger_count BETWEEN 2 AND 3 THEN 'Small Group'
-    WHEN passenger_count IN (4,6) THEN 'Medium Group'
+    WHEN passenger_count BETWEEN 4 AND 6 THEN 'Medium Group'
     WHEN passenger_count > 6 THEN 'Large Group'
     END AS trip_size_label,
     trip_distance, total_amount, store_and_fwd_flag
@@ -39,7 +41,10 @@ COPY (WITH yellow_first_cleanup AS
 ), 
 yellow_second_cleanup AS
  (
-    SELECT VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, UPPER(TRIM(store_and_fwd_flag)) store_and_fwd_flag,
+    SELECT VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, 
+    CASE WHEN UPPER(TRIM(store_and_fwd_flag)) IN ('Y','N') THEN UPPER(TRIM(store_and_fwd_flag))
+    ELSE NULL
+    END AS store_and_fwd_flag,
     ROUND(trip_distance,3) trip_distance,
     ROUND(total_amount,3) total_amount
     FROM yellow_first_cleanup
@@ -49,7 +54,7 @@ yellow_second_cleanup AS
     SELECT VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, CASE
     WHEN passenger_count = 1 THEN 'Solo'
     WHEN passenger_count BETWEEN 2 AND 3 THEN 'Small Group'
-    WHEN passenger_count IN (4,6) THEN 'Medium Group'
+    WHEN passenger_count BETWEEN 4 AND 6 THEN 'Medium Group'
     WHEN passenger_count > 6 THEN 'Large Group'
     END AS trip_size_label,
     trip_distance, total_amount, store_and_fwd_flag
@@ -58,18 +63,20 @@ yellow_second_cleanup AS
 
 --Main Query
 SELECT VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_size_label, trip_distance, total_amount, store_and_fwd_flag
-FROM range_of_customers)
+FROM range_of_customers
+--WHERE STRFTIME(tpep_pickup_datetime, '%Y') || '-Q' || DATE_PART('quarter', tpep_pickup_datetime) ='2023-Q1'
+)
 
 --Exporting data to a csv
 
-TO 'table cleaned_yellow_2023.csv' (HEADER, DELIMITER ',');
+TO 'cleaned_yellow_2023.csv' (HEADER, DELIMITER ',');
 
 --Read data from CSV into a new table
 DROP TABLE IF EXISTS cleaned_yellow_2023;
 
 CREATE TABLE cleaned_yellow_2023 AS
 SELECT * 
-FROM read_csv('C:/Users/Antavio/Documents/Phoenix Analytics/SQL Training/duckdb/table cleaned_yellow_2023.csv');
+FROM read_csv('C:/Users/Antavio/Documents/Phoenix Analytics/SQL Training/duckdb/cleaned_yellow_2023.csv');
 
 --Final Query
 SELECT LPAD(EXTRACT(MONTH FROM tpep_pickup_datetime)::TEXT, 3, '0') AS month_number, MONTHNAME(tpep_pickup_datetime) month_name, SUM(total_amount) monthly_revenue,
@@ -79,8 +86,19 @@ GROUP BY 1,2
 ORDER BY 1;
 
 --Question 3
-SELECT LPAD(EXTRACT(MONTH FROM tpep_pickup_datetime)::TEXT, 3, '0') AS month_number, MONTHNAME(tpep_pickup_datetime) month_name, VendorID, 
-ROUND(SUM(SUM(total_amount)) OVER(PARTITION BY VendorID),2) vendor_revenue, ROUND(SUM(total_amount),2) monthly_revenue, ROUND(vendor_revenue * 100/monthly_revenue,2) AS vendor_percentage
-FROM cleaned_yellow_2023
-GROUP BY 1,2,3
+WITH vendor_monthly_revenue AS
+(
+   SELECT LPAD(EXTRACT(MONTH FROM tpep_pickup_datetime)::TEXT, 3, '0') AS month_number, MONTHNAME(tpep_pickup_datetime) month_name, VendorID,
+   ROUND(SUM(total_amount),2) vendor_monthly_revenue
+   FROM cleaned_yellow_2023
+   GROUP BY 1,2,3
+)
+, totals AS (
+   SELECT month_number, month_name, VendorID, vendor_monthly_revenue,
+   ROUND(SUM(vendor_monthly_revenue) OVER (PARTITION BY month_number),2) AS total_monthly_revenue
+   FROM vendor_monthly_revenue
+)
+
+SELECT month_number, month_name, VendorID, vendor_monthly_revenue,total_monthly_revenue,ROUND(vendor_monthly_revenue/total_monthly_revenue*100,2) AS vendor_percent
+FROM totals
 ORDER BY 1;
